3bce251026a34464134c09a0fad387498592e058
Bruno Lustosa <lustosa@codezone.com.br>
2016-09-30 16:55:57 -0300

    preliminary support for webview / messenger extensions

--- class/Button.class.php
+++ class/Button.class.php
@@ -6,7 +6,7 @@ class Button {
     public $type;
     public $title;
 
-    public function __construct($type, $title, $payload) {
+    public function __construct($type, $title, $payload, $ext = false, $wv_height_ratio = 'compact') {
 
         if ($type != 'web_url' && $type != 'postback' && $type != 'phone_number') throw new Exception('Invalid type');
         if (empty($title)) throw new Exception('Empty button title');
@@ -15,6 +15,10 @@ class Button {
         if ($type == 'web_url'):
             if (!filter_var($payload, FILTER_VALIDATE_URL)) throw new Exception('Invalid URL');
             $this->url = $payload;
+            if ($ext):
+                $this->messenger_extensions = true;
+                $this->webview_height_ratio = $wv_height_ratio;
+            endif;
         else:
             if (empty($payload)) throw new Exception('Empty payload');
             $this->payload = $payload;
